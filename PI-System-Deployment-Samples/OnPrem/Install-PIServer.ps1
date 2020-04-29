937da6c3c187847978294128062c4ae0eace053c
Doug Broad <dbroad@osisoft.com>
2020-04-29 10:52:58 -0400

    Use +=

--- PI-System-Deployment-Samples/OnPrem/Install-PIServer.ps1
+++ PI-System-Deployment-Samples/OnPrem/Install-PIServer.ps1
@@ -272,7 +272,7 @@ function Install-SQLServerExpress() {
 
     if ($remote -eq $true) {
         # Install kit auto update does not work from remote PowerShell session
-        $params.Add("/UPDATEENABLED=FALSE")
+        $params += ("/UPDATEENABLED=FALSE")
         # Enable server role, otherwise may encounter error generating XML document
         Enable-WSManCredSSP -Role "Server"
     }
